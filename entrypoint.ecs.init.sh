ac69e55d7420fbf6ad4381e56169bc7ed1f29382
Meir Gabay <unfor19@gmail.com>
2022-01-22 05:00:16 +0200

    added messages

--- entrypoint.ecs.init.sh
+++ entrypoint.ecs.init.sh
@@ -8,9 +8,13 @@ _RESPONSE="$(aws ssm get-parameter --name "${AWS_CA_PARAMETER_NAME:-"/iamlive-do
     --output text \
     --query Parameter.Value || true)"
 if [[ "$_RESPONSE" =~ "-----BEGIN CERTIFICATE-----" ]]; then
+    echo "Fetched ca.pem from SSM Parameter store"
+    echo "Saving ca.pem ..."
     mkdir -p /certs
     echo "$_RESPONSE" > /certs/ca.pem
+    echo "Saved ca.pem successfully"
 else
+    echo "Failed to fetched ca.pem from SSM Parameter store"
     echo "$_RESPONSE"
     exit 1
 fi
