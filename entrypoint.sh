0532ed5e37c51174b1cf9d8872a686e92e76f7f4
Meir Gabay <unfor19@gmail.com>
2022-01-22 20:09:16 +0200

    changed allowed address to env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,12 @@ set -e
 set -o pipefail
 
 _FORMAT_LOGS="${FORMAT_LOGS:-"true"}"
+_ALLOWED_ADDRESS="${ALLOWED_ADDRESS:-"0.0.0.0"}"
 
 if [[ "$_FORMAT_LOGS" = "true" ]]; then
     /app/iamlive --output-file "/app/iamlive.log" \
-        --mode proxy --bind-addr "0.0.0.0:10080" $@ 2>&1 | jq -c .
+        --mode proxy --bind-addr "${_ALLOWED_ADDRESS}:10080" $@ | jq -c .
 else
     /app/iamlive --output-file "/app/iamlive.log" \
-        --mode proxy --bind-addr "0.0.0.0:10080" $@
+        --mode proxy --bind-addr "${_ALLOWED_ADDRESS}:10080" $@
 fi
