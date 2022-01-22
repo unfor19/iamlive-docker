445e62957cff36a80318ee8af0e4d30555b249d5
Meir Gabay <unfor19@gmail.com>
2022-01-22 05:07:20 +0200

    verbose logs

--- entrypoint.ecs.init.sh
+++ entrypoint.ecs.init.sh
@@ -3,16 +3,23 @@
 set -e
 set -o pipefail
 
-_RESPONSE="$(aws ssm get-parameter --name "${AWS_CA_PARAMETER_NAME:-"/iamlive-docker/certs/ca.pem"}" \
+_CERTS_DIR="${CERTS_DIR:-"/certs"}"
+_CA_PATH="${CA_PATH:-"${_CERTS_DIR}/ca.pem"}"
+_AWS_CA_PARAMETER_NAME="${AWS_CA_PARAMETER_NAME:-"/iamlive-docker/certs/ca.pem"}"
+
+echo "Fetching ${_AWS_CA_PARAMETER_NAME} ..."
+_RESPONSE="$(aws ssm get-parameter --name "$_AWS_CA_PARAMETER_NAME" \
     --with-decryption \
     --output text \
     --query Parameter.Value || true)"
 if [[ "$_RESPONSE" =~ "-----BEGIN CERTIFICATE-----" ]]; then
-    echo "Fetched ca.pem from SSM Parameter store"
-    echo "Saving ca.pem ..."
-    mkdir -p /certs
-    echo "$_RESPONSE" > /certs/ca.pem
-    echo "Saved ca.pem successfully"
+    echo "Fetched ${_AWS_CA_PARAMETER_NAME} from SSM Parameter store"
+    mkdir -p "$_CERTS_DIR"
+    echo "Saving ${_CA_PATH} ..."
+    echo "$_RESPONSE" > "$_CA_PATH"
+    wait
+    ls -lh "$_CA_PATH"
+    echo "Saved ${_CA_PATH} successfully"
 else
     echo "Failed to fetched ca.pem from SSM Parameter store"
     echo "$_RESPONSE"
