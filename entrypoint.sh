ed5a2c14792634822dd5e839d24fd413e171d2fb
Meir Gabay <unfor19@gmail.com>
2022-01-22 17:24:01 +0200

    format logs to fit cloudwatch as single stream

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,17 @@
 set -e
 set -o pipefail
 
-/app/iamlive --output-file "/app/iamlive.log" --mode proxy --bind-addr "0.0.0.0:10080" $@
+
+_FORMAT_LOGS="${FORMAT_LOGS:-"true"}"
+
+if [[ "$_FORMAT_LOGS" = "true" ]]; then
+    /app/iamlive --output-file "/app/iamlive.log" \
+        --mode proxy --bind-addr "0.0.0.0:10080" $@ 2>&1 | \
+            while IFS= read -r line; do
+                # Format logs
+                printf '[%s] %s\n' "$(date '+%Y-%m-%dT%H:%M:%S%LZ')" "$line"
+            done
+else
+    /app/iamlive --output-file "/app/iamlive.log" \
+        --mode proxy --bind-addr "0.0.0.0:10080" $@
+fi
