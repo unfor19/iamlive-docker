98e93913f34e8d443a84f6433d129ca971ba4a77
tinfoilcipher <39910469+tinfoilcipher@users.noreply.github.com>
2024-02-16 14:53:27 +0000

    feat: add output param to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,12 @@ set -o pipefail
 
 _FORMAT_LOGS="${FORMAT_LOGS:-"true"}"
 _ALLOWED_ADDRESS="${ALLOWED_ADDRESS:-"0.0.0.0"}"
+_OUTPUT_PATH="${_OUTPUT_PATH:-"/app/iamlive.log"}"
 
 if [[ "$_FORMAT_LOGS" = "true" ]]; then
-    /app/iamlive --output-file "/app/iamlive.log" \
+    /app/iamlive --output-file "${_OUTPUT_PATH}" \
         --mode proxy --bind-addr "${_ALLOWED_ADDRESS}:10080" $@ | jq -c .
 else
-    /app/iamlive --output-file "/app/iamlive.log" \
+    /app/iamlive --output-file "${_OUTPUT_PATH}" \
         --mode proxy --bind-addr "${_ALLOWED_ADDRESS}:10080" $@
 fi
